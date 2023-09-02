1d3581448b58a903a4ef597a2efb131feb2c94f2
AleXx815 <davgoga815@gmail.com>
2023-09-02 11:27:48 -0500

    first screen finished

--- lib/screens/basic_design/basic_design.dart
+++ lib/screens/basic_design/basic_design.dart
@@ -5,16 +5,19 @@ class BasicDesignScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Column(
         children: [
-          Image(
+          const Image(
             image: AssetImage("assets/dark_delirium.jpg"),
           ),
-          SizedBox(
-            height: 5,
-          ),
-          FirstInfo(),
+          const FirstInfo(),
+          const ButtonSection(),
+          Container(
+            margin: const EdgeInsets.all(30),
+            child: const Text(
+                "Inspirado en la historia bíblica del sacrificio de Isaac, la historia cuenta como Isaac intenta huir de su propia madre, siendo que ella cree que Dios le ha comandado a sacrificar a su hijo. The Binding of Isaac contiene una atmósfera oscura y humor escatólogico. Isaac tiene que atravesar todo tipo de lugares para huir mientras combate todo tipo de monstruos - desde demonios hasta pilas de popo vivas con caras sonrientes. Todo esto combinado como múltiples referencias a otras series de videojuegos y demás, y alusiones a trabajos previos de Edmund."),
+          )
         ],
       ),
     );
@@ -29,10 +32,7 @@ class FirstInfo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.symmetric(
-        horizontal: 30,
-        vertical: 30,
-      ),
+      margin: const EdgeInsets.all(30),
       child: const Row(
         children: [
           Column(
@@ -63,6 +63,64 @@ class FirstInfo extends StatelessWidget {
           Text("41"),
         ],
       ),
+    );
+  }
+}
+
+class ButtonSection extends StatelessWidget {
+  const ButtonSection({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 70, vertical: 10),
+      child: const Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          CustomButton(
+            icon: Icons.phone,
+            text: "CALL",
+          ),
+          CustomButton(
+            icon: Icons.location_on_rounded,
+            text: "ROUTE",
+          ),
+          CustomButton(
+            icon: Icons.share,
+            text: "SHARE",
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  final IconData icon;
+  final String text;
+
+  const CustomButton({
+    super.key,
+    required this.icon,
+    this.text = "",
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Icon(
+          icon,
+          color: Colors.blue,
+        ),
+        const SizedBox(height: 5),
+        Text(
+          text,
+          style: const TextStyle(color: Colors.blue),
+        ),
+      ],
     );
   }
 }
