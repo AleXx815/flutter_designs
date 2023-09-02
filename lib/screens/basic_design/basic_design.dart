ffa960347c81405deac0b9e078e26064241bd1c9
AleXx815 <davgoga815@gmail.com>
2023-09-02 10:49:28 -0500

    place and rating

--- lib/screens/basic_design/basic_design.dart
+++ lib/screens/basic_design/basic_design.dart
@@ -6,10 +6,62 @@ class BasicDesignScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Center(
-        child: Image(
-          image: AssetImage("assets/dark_delirium.jpg"),
-        ),
+      body: Column(
+        children: [
+          Image(
+            image: AssetImage("assets/dark_delirium.jpg"),
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          FirstInfo(),
+        ],
+      ),
+    );
+  }
+}
+
+class FirstInfo extends StatelessWidget {
+  const FirstInfo({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(
+        horizontal: 30,
+        vertical: 30,
+      ),
+      child: const Row(
+        children: [
+          Column(
+            // el mainAxisAlignment alinea los elementos de manera vertical
+            // el crossAxisAlignment alinea los elementos de manera horizontal
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Oeschinen Lake Campground",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                "Kandersteg, Switzerland",
+                style: TextStyle(color: Colors.grey),
+              ),
+            ],
+          ),
+          Spacer(),
+          Icon(
+            Icons.star,
+            color: Colors.red,
+          ),
+          Text("41"),
+        ],
       ),
     );
   }
