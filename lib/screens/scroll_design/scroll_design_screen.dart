379522ea68cd7ca2ebba65938b7bd3157e76be56
AleXx815 <davgoga815@gmail.com>
2023-09-04 21:23:38 -0500

    third page finished

--- lib/screens/scroll_design/scroll_design_screen.dart
+++ lib/screens/scroll_design/scroll_design_screen.dart
@@ -1,14 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class ScrollDesignScreen extends StatelessWidget {
   const ScrollDesignScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Stack(
-        children: [Background(), MainContent()],
+    return Scaffold(
+      body: PageView(
+        // physics: BouncingScrollPhysics(), FÍSICAS DE IOS
+
+        // CAMBIA LA DIRECCIÓN A LA CUAL HACER EL SCROLL
+        scrollDirection: Axis.vertical,
+        children: const [
+          FirstPage(),
+          SecondPage(),
+        ],
       ),
+    );
+  }
+}
+
+class FirstPage extends StatelessWidget {
+  const FirstPage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Stack(
+      children: [Background(), MainContent()],
     );
   }
 }
@@ -42,9 +63,9 @@ class MainContent extends StatelessWidget {
             ),
             Spacer(),
             Icon(
-              Icons.keyboard_arrow_down_rounded,
+              Icons.swipe_down,
               color: Colors.white,
-              size: 150,
+              size: 120,
             ),
           ],
         ),
@@ -61,12 +82,39 @@ class Background extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Color(0xff46b9d5),
+      color: const Color(0xff46b9d5),
       // SE LE DICE AL CONTAINER QUE TOME TOD EL ESPACIO POSIBLE
       height: double.infinity,
       alignment: Alignment.topCenter,
       child: const Image(
         image: AssetImage("assets/scroll-1.png"),
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  const SecondPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: const Color(0xff46b9d5),
+      child: Center(
+        child: ElevatedButton(
+          onPressed: () => context.go("/third-page"),
+          style: TextButton.styleFrom(
+            backgroundColor: const Color(0xff0098FA),
+            // shape: const StadiumBorder(),
+          ),
+          child: const Text(
+            "Next Page",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 30,
+            ),
+          ),
+        ),
       ),
     );
   }
